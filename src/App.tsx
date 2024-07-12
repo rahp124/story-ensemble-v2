bc51d1071314a8cf33d5d14747431cbd7f75e55c
michaelkplai <michael.kplai@gmail.com>
2024-07-11 23:11:05 -0400

    remove group feedback

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,12 @@ import SelectionToolbar from './components/SelectionToolbar';
 
 import { useStore } from './store';
 import { ImageIcon, ImageOff, Redo, Trash, Undo } from 'lucide-react';
-import {
-  Accordion,
-  Button,
-  Card,
-  Drawer,
-  ScrollArea,
-  Switch
-} from '@mantine/core';
+import { Button, Switch } from '@mantine/core';
 import { useShallow } from 'zustand/react/shallow';
-import { useNodeGroups } from './lib/useNodeGroups';
-import { useGroupFeedback } from './lib/useGroupFeedback';
 import {
   GenerationModal,
   GenerationModalProps
 } from './components/GenerationModal';
-import { NodeCountDisplayer } from './components/NodeCountDisplayer';
 import { ApiKeyModal } from './components/ApiKeyModal';
 import PersonaNode from './rf-components/PersonaNode';
 import ProblemNode from './rf-components/ProblemNode';
@@ -70,7 +60,6 @@ export default function App() {
     redo,
 
     selectedNodes,
-    selectNodes,
 
     setIterateModalOpen,
     setIterateModalTab,
@@ -95,7 +84,6 @@ export default function App() {
       redo: state.redo,
 
       selectedNodes: state.nodes.filter(({ selected }) => selected),
-      selectNodes: state.selectNodes,
 
       setIterateModalOpen: state.setIterateModalOpen,
       setIterateModalTab: state.setIterateModalTab,
@@ -119,11 +107,6 @@ export default function App() {
     if (hydrated) fitView();
   }, [fitView, hydrated]);
 
-  const { groupIdToNodeIds } = useNodeGroups();
-  const { groupFeedback, generateGroupFeedback } = useGroupFeedback();
-  const [generatingFeedback, setGeneratingFeedback] = useState(false);
-
-  const [feedbackDrawerOpened, setFeedbackDrawerOpened] = useState(false);
   const [showGenerationModal, setShowGenerationModal] = useState(false);
   const [generationType, setGenerationType] = useState<
     GenerationModalProps['generationType']
@@ -221,11 +204,6 @@ export default function App() {
               offLabel={<ImageOff className="w-3 h-3" />}
             />
           </div>
-        </Panel>
-        <Panel position="top-right">
-          <Button onClick={() => setFeedbackDrawerOpened(true)}>
-            View feedback
-          </Button>
         </Panel>
         <Controls position="bottom-right" showInteractive={false}>
           <ControlButton onClick={() => undo()}>
@@ -338,85 +316,6 @@ export default function App() {
         generationType={generationType}
       />
       <IterateModal />
-      <Drawer
-        opened={feedbackDrawerOpened}
-        onClose={() => setFeedbackDrawerOpened(false)}
-        title="Feedback"
-        position="right"
-        withOverlay={false}
-      >
-        <ScrollArea.Autosize scrollbars="y" maw="100%">
-          <div className="flex flex-col gap-4 w-full">
-            <Button
-              loading={generatingFeedback}
-              onClick={async () => {
-                setGeneratingFeedback(true);
-                await generateGroupFeedback();
-                setGeneratingFeedback(false);
-              }}
-            >
-              Generate feedback
-            </Button>
-            {Object.entries(groupIdToNodeIds).map(([groupId, nodeIds], idx) => {
-              const nodeIdsArr = [...nodeIds];
-
-              return (
-                <Card key={groupId} withBorder>
-                  <h3
-                    className="font-bold text-md mb-2 cursor-pointer"
-                    onClick={() => {
-                      selectNodes(nodeIdsArr);
-                      fitView({
-                        nodes: [...nodeIds].map((id) => ({ id })),
-                        duration: 1000,
-                        padding: 1.1
-                      });
-                    }}
-                  >
-                    Group {idx + 1}
-                  </h3>
-                  <NodeCountDisplayer nodeIds={nodeIdsArr} />
-
-                  {groupFeedback[groupId] && (
-                    <Accordion className="mt-4">
-                      {groupFeedback[groupId].map((feedback, idx) => (
-                        <Accordion.Item key={idx} value={`${idx}`}>
-                          <Accordion.Control>
-                            {feedback.feedbackSummary}
-                          </Accordion.Control>
-                          <Accordion.Panel>
-                            <NodeCountDisplayer
-                              nodeIds={feedback.affectedNodes}
-                            />
-
-                            <p className="mt-4">{feedback.feedback}</p>
-
-                            <Button
-                              className="mt-2"
-                              onClick={() => {
-                                selectNodes(feedback.affectedNodes);
-                                return fitView({
-                                  nodes: feedback.affectedNodes.map((id) => ({
-                                    id
-                                  })),
-                                  duration: 1000,
-                                  padding: 1.1
-                                });
-                              }}
-                            >
-                              Go to nodes
-                            </Button>
-                          </Accordion.Panel>
-                        </Accordion.Item>
-                      ))}
-                    </Accordion>
-                  )}
-                </Card>
-              );
-            })}
-          </div>
-        </ScrollArea.Autosize>
-      </Drawer>
       <ApiKeyModal />
     </div>
   );
--- src/store.ts
+++ src/store.ts
@@ -42,7 +42,6 @@ import { generateProblems, regenerateProblems } from './api/problems';
 import { generateIllustrativeImage } from './api/images';
 import debounce from 'lodash/debounce';
 import { cloneDeep, merge } from 'lodash';
-import { ConnectedFeedback, generateConnectedFeedback } from './api/feedback';
 import { WritableDraft } from 'immer';
 import {
   calculateDependentNodePositionAttributes,
@@ -180,15 +179,6 @@ type RFState = {
   copiedNodeIds: string[];
   copy: () => void;
   paste: () => void;
-
-  // Node Groups
-  groupIdToNodeIds: Record<string, string[]>;
-  groupIdToEdges: Record<string, { source: string; target: string }[]>;
-  nodeIdToGroupId: Record<string, string>;
-  calculateNodeGroups: () => void;
-
-  groupFeedback: Record<string, ConnectedFeedback>;
-  generateGroupFeedback: () => Promise<void>;
 };
 
 function partialize(state: RFState): Partial<RFState> {
@@ -1186,130 +1176,6 @@ const createStore: StateCreator<
         ],
         edges: [...get().edges, ...newEdges]
       });
-    },
-
-    groupIdToNodeIds: {} as Record<string, string[]>,
-    groupIdToEdges: {} as Record<string, { source: string; target: string }[]>,
-    nodeIdToGroupId: {} as Record<string, string>,
-
-    calculateNodeGroups: () => {
-      const groupIdToNodeIds: Record<string, Set<string>> = {};
-      const groupIdToEdges: Record<
-        string,
-        { source: string; target: string }[]
-      > = {};
-      const nodeIdToGroupId: Record<string, string> = {};
-
-      get().edges.forEach(({ source, target }) => {
-        const sourceGroupId = nodeIdToGroupId[source];
-        const sourceNodeIds = groupIdToNodeIds[sourceGroupId];
-        const targetGroupId = nodeIdToGroupId[target];
-        const targetNodeIds = groupIdToNodeIds[targetGroupId];
-
-        if (sourceGroupId && targetGroupId) {
-          if (sourceGroupId !== targetGroupId) {
-            for (const nodeId of targetNodeIds) {
-              nodeIdToGroupId[nodeId] = sourceGroupId;
-            }
-
-            groupIdToNodeIds[sourceGroupId] = new Set([
-              ...sourceNodeIds,
-              ...targetNodeIds
-            ]);
-            delete groupIdToNodeIds[targetGroupId];
-
-            groupIdToEdges[sourceGroupId] = [
-              { source, target },
-              ...groupIdToEdges[sourceGroupId],
-              ...groupIdToEdges[targetGroupId]
-            ];
-            delete groupIdToEdges[targetGroupId];
-          }
-        } else if (sourceGroupId && !targetGroupId) {
-          nodeIdToGroupId[target] = sourceGroupId;
-          groupIdToNodeIds[sourceGroupId] = new Set([...sourceNodeIds, target]);
-          groupIdToEdges[sourceGroupId].push({ source, target });
-        } else if (!sourceGroupId && targetGroupId) {
-          nodeIdToGroupId[source] = targetGroupId;
-          groupIdToNodeIds[targetGroupId] = new Set([...targetNodeIds, source]);
-          groupIdToEdges[targetGroupId].push({ source, target });
-        } else {
-          const newGroupId = nanoid();
-          nodeIdToGroupId[source] = newGroupId;
-          nodeIdToGroupId[target] = newGroupId;
-          groupIdToNodeIds[newGroupId] = new Set([source, target]);
-          groupIdToEdges[newGroupId] = [{ source, target }];
-        }
-      });
-
-      const groupIdToNodeIdsArray = Object.fromEntries(
-        Object.entries(groupIdToNodeIds).map(([groupId, nodeIds]) => [
-          groupId,
-          [...nodeIds]
-        ])
-      );
-
-      set({
-        groupIdToNodeIds: groupIdToNodeIdsArray,
-        groupIdToEdges,
-        nodeIdToGroupId
-      });
-    },
-
-    groupFeedback: {} as Record<string, ConnectedFeedback>,
-    generateGroupFeedback: async () => {
-      const { groupIdToNodeIds, nodes, groupIdToEdges } = get();
-      return Promise.all(
-        Object.keys(groupIdToNodeIds).map(async (groupId) => {
-          const nodeIds = [...groupIdToNodeIds[groupId]];
-          const nodeContent = nodeIds
-            .map((nodeId) => nodes.find((node) => node.id === nodeId))
-            .filter((node) => node !== undefined)
-            .map((node) => {
-              if (node.type === NodeType.Storyboard) {
-                const storyboard: StoryboardNodeData['storyboard'] =
-                  node.data.storyboard;
-                const sanitizedStoryboard = {
-                  title: storyboard.title,
-                  outline: storyboard.outline.map((frame) => ({
-                    frameType: frame.frameType,
-                    description: frame.description,
-                    caption: frame.caption
-                  }))
-                };
-
-                return {
-                  id: node.id,
-                  type: node.type,
-                  content: JSON.stringify(sanitizedStoryboard)
-                };
-              } else {
-                return {
-                  id: node.id,
-                  type: node.type,
-                  content: node.data.content
-                };
-              }
-            });
-          const edges = groupIdToEdges[groupId];
-
-          const feedbacks = await generateConnectedFeedback(
-            nodeContent,
-            edges
-          ).then((feedbacks) =>
-            feedbacks.filter(({ affectedNodes }) =>
-              affectedNodes.every((nodeId) => nodeIds.includes(nodeId))
-            )
-          );
-
-          return [groupId, feedbacks] as const;
-        })
-      )
-        .then((groupFeedback) => {
-          const groupIdToFeedbacks = Object.fromEntries(groupFeedback);
-          return groupIdToFeedbacks;
-        })
-        .then((groupFeedback) => set({ groupFeedback }));
     }
   };
 };
