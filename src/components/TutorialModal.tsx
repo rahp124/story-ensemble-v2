3c0c239fcd8ebc5a548f467344da907080a5354d
Sangho <sh31659@gmail.com>
2024-07-31 05:37:11 -0400

    Fix error resulting from attributes in iframe not styled for React

--- src/components/TutorialModal.tsx
+++ src/components/TutorialModal.tsx
@@ -35,7 +35,7 @@ export function TutorialModal() {
                         <div className='text-center' style={{
                                 textAlign: 'center'
                                 }}>
-                            <iframe width="560" height="315" src={url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            <iframe width="560" height="315" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
                             </iframe>
                         </div>
                     </>
