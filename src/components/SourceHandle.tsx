55707c69406fd789d2aa19ff98ec2f5a489efa43
michaelkplai <michael.kplai@gmail.com>
2024-08-05 18:30:01 +0200

    revert to simple target handles

--- src/components/SourceHandle.tsx
+++ src/components/SourceHandle.tsx
@@ -10,7 +10,7 @@ export default function SourceHandle(
       position={Position.Bottom}
       className={cn(
         props.className,
-        'w-[20px] h-[20px] -bottom-[10px] bg-gray-50 border-gray-400 border-2'
+        'size-[20px] -bottom-[10px] bg-gray-50 border-gray-400 border-2'
       )}
       {...props}
     />
--- src/components/TargetHandle.tsx
+++ src/components/TargetHandle.tsx
@@ -1,32 +1,17 @@
 import { cn } from '@/lib/utils';
-import { useStore } from '@/store';
 import { Handle, HandleProps, Position } from 'reactflow';
 
 export default function TargetHandle(
   props: Partial<HandleProps> & { className?: string }
 ) {
-  const connectionInProgress = useStore((state) => state.connectionInProgress);
-
   return (
     <Handle
       type="target"
       position={Position.Top}
       className={cn(
         props.className,
-        `w-full h-full absolute top-1/2 left-1/2 opacity-0 rounded-none -translate-x-1/2 -translate-y-1/2 z-50`
+        'size-[20px] -top-[10px] bg-gray-50 border-gray-400 border-2'
       )}
-      isConnectableEnd={true}
-      style={
-        !connectionInProgress
-          ? {
-              width: 1,
-              height: 1
-            }
-          : {
-              width: '100%',
-              height: '100%'
-            }
-      }
       {...props}
     />
   );
