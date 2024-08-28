7c43a610784e7edbdfb973046704d83248dd104e
Sangho <sh31659@gmail.com>
2024-08-28 15:39:05 -0700

    Correct tutorial video link

--- src/data/tutorial.tsx
+++ src/data/tutorial.tsx
@@ -297,7 +297,7 @@ export const tutorialMenuData: tutorialMenuItem = {
         },
         {
             "title": "Propagate changes ↑",
-            "url": "https://www.youtube.com/embed/tdL4VCWEejc",
+            "url": "https://www.youtube.com/embed/VkUS4Q71V_o",
             "left_section": <ArrowUpFromLineIcon style={{ width: rem(14), height: rem(14) }}/>,
             "right_section": 
             <Text size="xs" c="dimmed">
